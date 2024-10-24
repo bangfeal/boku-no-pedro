b44b1916e81b5109417035370e4ad9a0ff8389fe
Jebohs <romanpal0404@gmail.com>
2024-10-24 17:37:57 +0200

    ChatGPT made some ideas, now sure how I feel about this might revert to main branch and re-write

--- src/ChoiceButtonLogic.java
+++ src/ChoiceButtonLogic.java
@@ -1,3 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
 public class ChoiceButtonLogic {
-    // todo IMPLEMENT LOGIC FOR CHOICE BUTTONS
+
+    public static JButton createChoiceButton(SceneLoaderGUI gui, Choice choice) {
+        return new ChoiceButton(gui, choice);
+    }
+
+    private static class ChoiceButton extends AbstractGameButton {
+        private final SceneLoaderGUI gui;
+        private final Choice choice;
+
+        public ChoiceButton(SceneLoaderGUI gui, Choice choice) {
+            super(choice.getText());
+            this.gui = gui;
+            this.choice = choice;
+        }
+
+        @Override
+        protected void onClick() {
+            // Load the new scene when the button is clicked
+            Scene newScene = new Scene(choice.getScene());
+            gui.loadNewScene(newScene);
+        }
+    }
 }
--- src/SceneLoaderGUI.java
+++ src/SceneLoaderGUI.java
@@ -7,33 +7,47 @@ import java.io.File;
 import java.io.IOException;
 
 public class SceneLoaderGUI extends JPanel {
-    JFrame frame;
-    Scene scene;
+    private JFrame frame;
+    private Scene scene;
+    private JPanel choicePanel;
 
     // Uses a default 16:9 aspect ratio
-    int width = 1920;
-    int height = width * 9 / 16;
+    private final int width = 1920;
+    private final int height = width * 9 / 16;
+    private boolean clicked = false;
 
-    boolean clicked = false;
-
-    SceneLoaderGUI(JFrame frame, Scene scene) {
-        this.scene = scene;
+    public SceneLoaderGUI(JFrame frame, Scene scene) {
         this.frame = frame;
+        this.scene = scene;
+        initializeGUI();
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (!scene.getCurrentDialog().hasChoice()) {
+                    clicked = true;
+                    repaint();
+                }
+            }
+        });
+    }
+
+    private void initializeGUI() {
         frame.getContentPane().removeAll();
         frame.setSize(width, height);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(this);
 
-        this.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                clicked = true;
-                repaint();
-            }
-        });
+        choicePanel = new JPanel();
+        choicePanel.setLayout(new FlowLayout());
+        choicePanel.setOpaque(false); // Make the panel transparent
+        choicePanel.setVisible(false); // Initially hide it until choices appear
+
+        this.setLayout(new BorderLayout());
+        this.add(choicePanel, BorderLayout.SOUTH);
     }
-
 
     @Override
     public void paintComponent(Graphics g) {
@@ -49,13 +63,42 @@ public class SceneLoaderGUI extends JPanel {
                 graphics.drawImage(ImageIO.read(new File("text_box.png")), 450, 700, this);
                 graphics.drawString(scene.getCurrentDialog().getText(), 545, 800);
 
-                if (clicked) {
-                    scene.increment();
-                    clicked = false;
+                if (scene.getCurrentDialog().hasChoice()) {
+                    showChoiceButtons();
+                } else {
+                    hideChoiceButtons();
+                    if (clicked) {
+                        scene.increment();
+                        clicked = false;
+                    }
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void showChoiceButtons() {
+        choicePanel.removeAll();
+
+        ChoiceDialog choiceDialog = (ChoiceDialog) scene.getCurrentDialog();
+        JButton choice0 = ChoiceButtonLogic.createChoiceButton(this, choiceDialog.choice0);
+        JButton choice1 = ChoiceButtonLogic.createChoiceButton(this, choiceDialog.choice1);
+
+        choicePanel.add(choice0);
+        choicePanel.add(choice1);
+        choicePanel.setVisible(true);
+        revalidate();
+        repaint();
+    }
+
+    private void hideChoiceButtons() {
+        choicePanel.setVisible(false);
+    }
+
+    public void loadNewScene(Scene newScene) {
+        this.scene = newScene;
+        repaint();
     }
 }
